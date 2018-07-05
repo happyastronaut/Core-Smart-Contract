7874653fa9fb738a91c5a7bd5a19730703744862
Vladyslav <jason.soc@ukr.net>
2018-07-05 18:04:43 +0200

    remove task impl

--- CoreContractV3/Contract1.cs
+++ CoreContractV3/Contract1.cs
@@ -7,106 +7,123 @@ namespace CoreContractV3
 {
     public class Contract1 : SmartContract
     {
+        //params: 0710
+        //return: 05
+
+        public static readonly byte[] team = "AK9cP6LdpjuaQguvahFA7ojMiDbt4gEdAK".ToScriptHash();
         public static Object Main(string action, params object[] args)
         {
-            //TODO fix bug when ended ask wasnt start and got removed
-            //params 0710
-            //ret: 05
-
             if (Runtime.Trigger == TriggerType.Application)
             {
                 if (action == "name")
                 {
                     return "Core contract v3";
                 }
-                if (action == "getcorrcounter")
-                {
-                    return GetCorrCounter();
-                }
-                if (action == "getcounter")
-                {
-                    return GetCounter();
-                }
-                if (action == "registertask")
+                else if (action == "registertask")
                 {
                     byte[] address = (byte[])args[0];
                     byte[] ip = (byte[])args[1];
                     return RegisterTask(address, ip);
                 }
-                if (action == "endtask")
-                {
-                    return EndTask();
-                }
-                if (action == "gettask")
+                else if (action == "gettask")
                 {
                     return GetTask();
                 }
-                if (action == "init")
+                else if (action == "finishtask")
+                {
+                    byte[] address = (byte[])args[0];
+                    return FinishTask(address);
+                }
+                else if (action == "removetask")
+                {
+                    byte[] address = (byte[])args[0];
+                    byte[] id = (byte[])args[1];
+                    return RemoveTask(address, id);
+                }
+                else if (action == "init")
                 {
                     return Init();
                 }
             }
-            return false;                
+            return false;
         }
-        
+
         private static object Init()
         {
-            var counter = Storage.Get(Storage.CurrentContext, "counter").AsBigInteger();
-            var curr = Storage.Get(Storage.CurrentContext, "current").AsBigInteger();
-            BigInteger init_value_curr = 1;
-            BigInteger init_value_counter = 0;
-            if (counter == 0 || curr == 0)
+            var id = Storage.Get(Storage.CurrentContext, "id").AsBigInteger();
+            var count = Storage.Get(Storage.CurrentContext, "count").AsBigInteger();
+            BigInteger init_value_id = 1;
+            BigInteger init_value_count = 0;
+            if (id == 0 || count == 0)
             {
-                Storage.Put(Storage.CurrentContext, "counter", init_value_curr);
-                Storage.Put(Storage.CurrentContext, "current", init_value_counter);
+                Storage.Put(Storage.CurrentContext, "id", init_value_id);
+                Storage.Put(Storage.CurrentContext, "count", init_value_count);
                 return true;
             }
             return false;
         }
 
-        private static object EndTask()
+
+        /* Tasks */
+        private static object RegisterTask(byte[] address, byte[] ip)
         {
-            IncrementCurrCounter();
+            if (!Runtime.CheckWitness(address)) return false;
+            IncrementCount();
+            var id = GetCount();
+            Storage.Put(Storage.CurrentContext, id.ToByteArray(), ip);
+            Storage.Put(Storage.CurrentContext, ip, address);
             return true;
         }
 
         private static object GetTask()
         {
-            var curr = GetCorrCounter().ToByteArray();
-            return Storage.Get(Storage.CurrentContext, curr);
+            var id = GetID().ToByteArray();
+            return Storage.Get(Storage.CurrentContext, id);
         }
 
-        private static object RegisterTask(byte[] address, byte[] ip)
+        private static object FinishTask(byte[] from)
         {
-            IncrementCounter();
-            var counter = GetCounter();  
-            Storage.Put(Storage.CurrentContext, counter.ToByteArray(), ip);
-            Storage.Put(Storage.CurrentContext, counter.ToByteArray(), address);
+            if (!Runtime.CheckWitness(from) || !Runtime.CheckWitness(team)) return false;
+            BigInteger id = 0;
+            do
+            {
+                IncrementID();
+                id = GetID();
+            }
+            while (Storage.Get(Storage.CurrentContext, id.AsByteArray()) == "null".AsByteArray());
             return true;
         }
 
-        private static BigInteger GetCounter()
+        private static object RemoveTask(byte[] from, byte[] id)
         {
-            return Storage.Get(Storage.CurrentContext, "counter").AsBigInteger();
+            if (!Runtime.CheckWitness(from)) return false;
+            Storage.Put(Storage.CurrentContext, id, "null".AsByteArray());
+            return true;
         }
 
-        private static BigInteger GetCorrCounter()
+        /* counters */
+        private static BigInteger GetCount()
         {
-            return Storage.Get(Storage.CurrentContext, "current").AsBigInteger();
+            return Storage.Get(Storage.CurrentContext, "count").AsBigInteger();
         }
 
-        private static void IncrementCurrCounter()
+        private static BigInteger GetID()
         {
-            var counter = Storage.Get(Storage.CurrentContext, "current").AsBigInteger();
-            counter = counter + 1;
-            Storage.Put(Storage.CurrentContext, "current", counter);
+            return Storage.Get(Storage.CurrentContext, "id").AsBigInteger();
         }
 
-        private static void IncrementCounter()
+        private static void IncrementCount()
         {
-            var counter = Storage.Get(Storage.CurrentContext, "counter").AsBigInteger();
-            counter = counter + 1;
-            Storage.Put(Storage.CurrentContext, "counter", counter);
+            var count = GetCount();
+            count = count + 1;
+            Storage.Put(Storage.CurrentContext, "count", count);
+        }
+
+        private static void IncrementID()
+        {
+            var id = GetID();
+            id = id + 1;
+            Storage.Put(Storage.CurrentContext, "id", id);
         }
     }
 }
